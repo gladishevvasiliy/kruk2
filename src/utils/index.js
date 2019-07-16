b6f4dbe1c302ac2faefdcdda5c2899b1a5066a7e
Vasiliy Gladishev <gladishevvasiliy@gmail.com>
2019-07-16 16:20:32 +0300

    add saving settings and saving with date

--- src/utils/index.js
+++ src/utils/index.js
@@ -26,3 +26,10 @@ export const getDataFromServer = (url) =>
     const data = resp.json()
     return data
   })
+
+export const getDate = () => {
+  const today = new Date()
+  return `${String(today.getMonth() + 1).padStart(2, '0')}. ${String(
+    today.getDate(),
+  ).padStart(2, '0')}.${today.getFullYear()}`
+}
